213a74e4e2bc1ec779f953ccbc1b345d01f7e953
medbm5 <medbaamrani0@gmail.com>
2020-10-10 14:02:20 +0100

    first commit

---
+++ src/Hompage.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+
+const Hompage=()=>{
+
+    return(
+<div className="homepage">
+<div className="directory-menu">
+    <div className="menu-item">
+        <div className="content">
+            <h1>HATS</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        <div className="content">
+            <h1>JAKETS</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        <div className="content">
+            <h1>SNEAKERS</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        <div className="content">
+            <h1>WOMENS</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+    <div className="menu-item">
+        <div className="content">
+            <h1>MENS</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+</div>
+</div>
+
+    )
+
+}
+
+export default Hompage;

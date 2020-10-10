7d72279bf36392f3d4e0ffc6f2b9576f42bb416d
medbm5 <medbaamrani0@gmail.com>
2020-10-10 22:54:17 +0100

    styling directory and menu item

--- src/pages/Homepage/Hompage.component.jsx
+++ src/pages/Homepage/Hompage.component.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import './homepage.style.scss'
+import Directory from '../../components/directory/directory.component'
 
 const Hompage=()=>{
 
     return(
 <div className="homepage">
-<div className="directory-menu">
-    <div className="menu-item">
-        <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-    <div className="menu-item">
-        <div className="content">
-            <h1 className="title">JAKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-    <div className="menu-item">
-        <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-    <div className="menu-item">
-        <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-    <div className="menu-item">
-        <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
-    </div>
-</div>
+        <Directory/>
 </div>
 
     )

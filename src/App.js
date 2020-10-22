f3c3c89518d3d42453d2bb171146355c81b78842
medbm5 <medbaamrani0@gmail.com>
2020-10-22 14:35:25 +0100

    preview collections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/Homepage/Hompage.component'
+import Homepage from './pages/HomePage/homepage.component'
+import {Switch ,Route} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-    <Homepage/>
+      <Switch>
+      <Route exact path='/' component={Homepage}/>
+      <Route exact path='/shop' component={ShopPage}/>
+
+    </Switch>
     </div>
   );
 }

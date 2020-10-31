df50bc0388d50ea69b8ca878c9a2c6ce9ee6c0f5
medbm5 <medbaamrani0@gmail.com>
2020-10-31 15:13:31 +0100

    collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,21 @@
 
-import React, { Component } from 'react'
-import SHOP_DATA from './shop.data'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-
-export default class ShopPage extends Component {
-    constructor(props){
-        super(props);
-
-        this.state={
-            collections:SHOP_DATA
-        }
-
-    }
+import React from 'react'
+import {Route} from 'react-router-dom'
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import './shop.styles.scss'
+import CollectionPage from '../collection/collection.component'
 
 
-    render() {
-        const {collections}=this.state
+const ShopPage=({match})=> {
+    
         return (
             <div className='shop-page'>
-
-                {collections.map(({id ,...otherCollectionProps}) =>(
-                    <CollectionPreview key={id} {...otherCollectionProps}/>
-                ))}
+                <Route exact path={`${match.path}`} component={CollectionOverview} />
+                <Route path={`${match.path}/:collectionId`}component={CollectionPage} />
             </div>
         )
-    }
 }
 
+
+
+export default ShopPage;
